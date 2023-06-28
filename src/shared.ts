615167a12ebc533d01a13148029444635549f994
Marc Hassan <marc.j.hassan@gmail.com>
2023-06-27 20:27:34 -0400

    Add ceiling option (#2)
    This PR adds a `ceiling` option, with default `true`; when `false` is
    passed, `Math.ceil(quota)` is skipped.
    
    Resolves https://github.com/lujjjh/node-cpu-count/issues/1.

--- src/shared.ts
+++ src/shared.ts
@@ -1,6 +1,6 @@
 import { cpus } from "os";
 
-export const normalizeQuota = (quota: number | undefined, min: number, max: number) => {
+export const normalizeQuota = (quota: number | undefined, min: number, max: number, ceiling: boolean = true) => {
   if (quota === undefined || !Number.isFinite(quota)) return max;
-  return Math.min(Math.max(min, Math.ceil(quota)), max);
+  return Math.min(Math.max(min, ceiling ? Math.ceil(quota) : quota), max);
 };
